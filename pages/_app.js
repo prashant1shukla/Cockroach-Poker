2e40b5764ebf62bce5707258f4f7dcbdaf7b637f
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2023-04-12 17:01:49 +0530

    Home-Create team join team

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,30 @@
 import '@/styles/globals.css'
+// new 
+// import './App.css';
+// import Rules from '../Components/Rules';
+// import Game from '../Components/Game';
+// import Home from '../Components/Home';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+// new
+
+// const App = () => {
+//   return (
+//     <div className="App">
+  //     <Routes>
+  //     <Route path='/' element={<Home />} />
+  //     <Route path='/rules' element={<Rules />} />
+  //     <Route path='/game1' element={<Game2 />} />
+  //     <Route path='/rules2' element={<Rules2 />} />
+  //     <Route path='/game2' element={<Game />} />
+  //     <Route path='/rules3' element={<Rules3 />} />
+  //     <Route path='/game3' element={<Game3 />} />
+  //   </Routes>
+  // </div>
+//   )
+// }
+
+// export default App
